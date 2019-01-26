129c3cebf47b7fe8b41a99c05bce41efd6478a35
Katarzyna Kańska <katarzyna.m.kanska@gmail.com>
2019-01-26 15:16:52 +0100

    Very basic TP, FP, TN, FN computation

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,10 @@
     />
     <p>Points: {{points.length}}</p>
     <p>Tiles up: {{tilesUp}}</p>
+    <p>True Positives: {{truePositives}}</p>
+    <p>False Positives: {{falsePositives}}</p>
+    <p>True Negatives: {{trueNegatives}}</p>
+    <p>False Negatives: {{falseNegatives}}</p>
   </div>
 </template>
 
@@ -18,6 +22,8 @@ const n = 50;
 function range(size, startAt = 0) {
     return [...Array(size).keys()].map(i => i + startAt);
 }
+
+const tileSize = 1;
 
 const tiles = range(k * k)
   .map((i) => {
@@ -40,6 +46,43 @@ const points = range(n)
   };
 });
 
+function isPointInsideTile(point, tile) {
+  return tile.x <= point.x && point.x < tile.x + tileSize
+    && tile.y <= point.y && point.y < tile.y + tileSize;
+}
+
+function truePositive(point, tile) {
+  return point.v && (tile.v === point.v);
+}
+
+function falsePositive(point, tile) {
+  return !point.v && (tile.v !== point.v);
+}
+
+function trueNegative(point, tile) {
+  return !point.v && (tile.v === point.v);
+}
+
+function falseNegative(point, tile) {
+  return point.v && (tile.v !== point.v);
+}
+
+function computeMetric(points, tiles, metric) {
+  return points.reduce(
+    function (counter, point) {
+      let isCorrect = false;
+      tiles.forEach(
+        function(tile) {
+          if (isPointInsideTile(point, tile)) {
+            isCorrect = metric(point, tile);
+          }
+        }
+      );
+      return counter + isCorrect;
+    },
+    0);
+}
+
 export default {
   name: 'app',
   components: {
@@ -58,6 +101,18 @@ export default {
         (d) => counter += d.v
       );
       return counter;
+    },
+    truePositives: function() {
+        return computeMetric(this.points, this.tiles, truePositive);
+    },
+    falsePositives: function() {
+        return computeMetric(this.points, this.tiles, falsePositive);
+    },
+    trueNegatives: function() {
+        return computeMetric(this.points, this.tiles, trueNegative);
+    },
+    falseNegatives: function() {
+        return computeMetric(this.points, this.tiles, falseNegative);
     }
   }
 }
